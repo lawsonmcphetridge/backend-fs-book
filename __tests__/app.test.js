cfe361d7065ffe4a8fab8d34eebf791a8a9aeab1
Lawson <lawsonmcphetridge2020@gmail.com>
2022-11-08 15:13:24 -0800

    wrote book test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,14 +2,25 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 // const request = require('supertest');
 // const app = require('../lib/app');
+const { Book } = require('../lib/models/bookModel');
+const { request } = require('express');
+const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+
+describe('tests', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+
+  it('/books will list out a page of all books', async () => {
+    const res = await request(app).get('/books');
+    const books = await Book.getAllBooks();
+    expect(res.body).toEqual(books);
   });
+
+
+
   afterAll(() => {
     pool.end();
   });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,42 +1,33 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS books;
+
 DROP TABLE IF EXISTS authors;
 
 CREATE TABLE books (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     title VARCHAR,
-    released DATE
+    released BIGINT
 );
 
 CREATE TABLE authors (
-  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  name VARCHAR,
-  dob DATE,
-  pob VARCHAR
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR,
+    dob BIGINT,
+    pob VARCHAR
 );
 
-INSERT INTO books (
-    title,
-    released
-)
-
-VALUES (
-    ('Ready Player One', 2011-08-16),
-    ('Harry Potter', 1997-06-26),
-    ('Lord of the rings', 1954-07-29),
-    ('Moby Dick', 1851-10-18),
-)
-INSERT INTO authors (
-    name,
-    dob,
-    pob
-)
-
-VALUES (
-    ('Ernest Cline', 1972-03-39, 'Ashland, OH'),
-    ('J. K. Rowling', 1965-07-31, 'Yate, United Kingdom'),
-    ('J. R. R. Tolkien', 1892-01-3, 'Bloemfontein, South Africa'),
-    ('Herman Melville', 1819-08-1, 'New York, NY')
-)
-;
+INSERT INTO
+    books (title, released)
+VALUES   
+        ('Ready Player One', 2011),
+        ('Harry Potter', 1997),
+        ('Lord of the rings', 1954),
+        ('Moby Dick', 1851);
+INSERT INTO
+    authors (name, dob, pob)
+VALUES
+        ('Ernest Cline', 1972, 'Ashland, OH'),
+        ('J. K. Rowling', 1965, 'Yate, United Kingdom'),
+        ('J. R. R. Tolkien', 1892, 'Bloemfontein, South Africa'),
+        ('Herman Melville', 1819, 'New York, NY');
