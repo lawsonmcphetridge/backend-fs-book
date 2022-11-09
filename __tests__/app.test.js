ffbcbac55c0c46ca15daec55e7c06961fec088dd
Lawson <lawsonmcphetridge2020@gmail.com>
2022-11-09 09:19:44 -0800

    wrote 3rd test to test authors page

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,10 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
-// const app = require('../lib/app');
+
 const { Book } = require('../lib/models/bookModel');
 const app = require('../lib/app');
+const { Author } = require('../lib/models/authorModel');
 
 
 describe('tests', () => {
@@ -26,6 +27,14 @@ describe('tests', () => {
     const res = await request(app).get('/books/1');
     const exp = await Book.getSingleBook('1');
     expect(res.body).toEqual(exp);
+  });
+
+
+
+  it('/authors will list a page of authors', async () => {
+    const res = await request(app).get('/authors');
+    const expected = await Author.getAllAuthors();
+    expected(res.body).toEqual(expected);
   });
 
 
